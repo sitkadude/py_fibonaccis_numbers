61e0aa4b45cb2bbf517ea58f39f948a1b95c7c66
Lukas Jorgensen <lukas.r.jorgensen@gmail.com>
2019-12-17 09:59:48 -0500

    first commit, functioning

---
+++ fibonaccis_numbers.py
@@ -0,0 +1,17 @@
+def fib(n):
+    if n < 1:
+         return None
+    if n < 3:
+        return 1
+
+    e1 = e2 = 1
+    sum = 0
+    
+    for i in range(3, n + 1):
+        sum = e1 + e2
+        e1, e2 = e2, sum
+    return sum
+
+num = int(input("Pick a number: "))
+for n in range(1, (num + 1)): 
+    print(n, "-->", fib(n))
